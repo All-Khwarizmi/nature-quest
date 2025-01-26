44cae7d63e259e73246f9b2316ed3e1b87c38179
JASON SUAREZ <suarezjason@icloud.com>
2025-01-26 11:17:28 +0100

    feat: implement save user upload on db

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -12,15 +12,18 @@ import { QuestCard } from "~~/components/quest-card";
 import { Alert, AlertDescription, AlertTitle } from "~~/components/ui/alert";
 import { Button } from "~~/components/ui/button";
 import { Progress } from "~~/components/ui/progress";
+import addUpload from "~~/src/actions/uploadActions";
+import addUser, { getUser } from "~~/src/actions/userActions";
+import { User } from "~~/src/db/schema";
 
 export default function Home() {
   // Get the user's wallet address and connection status
   const { address, isConnected } = useAccount();
 
   // State to store the uploaded image blob
-  const [blob, setBlob] = useState<PutBlobResult | null>(null);
-  const [error, setError] = useState<string | null>(null);
   const [progress, setProgress] = useState<number>(0);
+  const [error, setError] = useState<string | null>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
   const [isUploading, setIsUploading] = useState<boolean>(false);
 
   const handleUpload = async (imageFile: File) => {
@@ -35,25 +38,24 @@ export default function Home() {
     setError(null);
     setProgress(0);
 
-    try {
-      const newBlob = await upload(imageFile.name, imageFile, {
-        access: "public",
-        handleUploadUrl: "/api/image/upload",
-        clientPayload: address,
-        contentType: imageFile.type,
-        onUploadProgress: (progress: UploadProgressEvent) => {
-          console.log("Progress:", progress);
-          setProgress(Math.round((progress.loaded / progress.total) * 100));
-        },
-      });
-
-      setBlob(newBlob);
-    } catch (err) {
-      setError("Failed to upload image. Please try again.");
-      console.error(err);
-    } finally {
-      setIsUploading(false);
+    // Get user from postgres
+    const user = await handleGetUser(address);
+    if (!user) {
+      console.log("No user");
+      return;
     }
+
+    // Store image blob
+    const blob = await handleUploadBlob(imageFile, address);
+    if (!blob) {
+      console.log("No blob");
+      return;
+    }
+
+    // Upload user data to postgres
+    await handleUploadUserData(user, blob);
+
+    // TODO: upload business data?
   };
 
   // Handle the image capture event
@@ -61,7 +63,7 @@ export default function Home() {
     // Pass to next module (MobileNet, upload, etc.)
     // 👉 agent goes here
 
-    handleUpload(imageFile);
+    await handleUpload(imageFile);
   };
 
   const handleRetry = () => {
@@ -117,4 +119,78 @@ export default function Home() {
       </div>
     </div>
   );
+
+  async function handleGetUser(address: string) {
+    try {
+      // Get the user Id from the db
+      console.log("Getting user data");
+      let user = await getUser(address);
+      console.log(user);
+      if (!user) {
+        console.log("User not found, creating new user");
+        user = await addUser(address);
+        if (!user) {
+          setError("Failed to create user. Please try again.");
+          return;
+        }
+        return user;
+      }
+      return user;
+    } catch (error) {
+      console.error(error);
+      setError("Failed to upload user data. Please try again.");
+      return;
+    }
+  }
+
+  async function handleUploadBlob(imageFile: File, address: string) {
+    try {
+      const newBlob = await upload(imageFile.name, imageFile, {
+        access: "public",
+        handleUploadUrl: "/api/image/upload",
+        clientPayload: address,
+        contentType: imageFile.type,
+        onUploadProgress: (progress: UploadProgressEvent) => {
+          console.log("Progress:", progress);
+          setProgress(Math.round((progress.loaded / progress.total) * 100));
+        },
+      });
+
+      console.log("new blob", newBlob);
+
+      setBlob(newBlob);
+      return newBlob;
+    } catch (err) {
+      setError("Failed to save image. Please try again.");
+      console.error(err);
+    } finally {
+      setIsUploading(false);
+    }
+  }
+
+  async function handleUploadUserData(user: User, newBlob: PutBlobResult) {
+    try {
+      console.log("Uploading user data");
+      // Update the user's db entry
+      const uploadResult = await addUpload({
+        userId: user.id,
+        classificationJson: JSON.stringify(newBlob), // TODO: Update this
+        imageUrl: newBlob.url,
+        metadata: "Spring Birds", // TODO: Update this
+        status: "pending", // TODO: Update this
+        location: [0, 0], // TODO: Update this
+        season: "spring", // TODO: Update this
+        createdAt: new Date(),
+        photoTakenAt: new Date(),
+        updatedAt: new Date(),
+        questId: user.id, // TODO: Update this
+      });
+
+      console.log(uploadResult);
+      return uploadResult;
+    } catch (error) {
+      console.error(error);
+      setError("Failed to upload user data. Please try again.");
+    }
+  }
 }
