74e0838f545af0ef4bbe55c1094dd3e0ee5df127
JASON SUAREZ <suarezjason@icloud.com>
2025-01-26 10:37:53 +0100

    refactor: update actions to match requirements

--- packages/nextjs/src/actions/uploadActions.ts
+++ packages/nextjs/src/actions/uploadActions.ts
@@ -4,8 +4,8 @@ import { db } from "../db/drizzle";
 import { Upload, uploads } from "../db/schema";
 import { eq } from "drizzle-orm";
 
-export default async function addUpload(upload: Upload) {
-  return db.insert(uploads).values(upload);
+export default async function addUpload(upload: Omit<Upload, "id">) {
+  return (await db.insert(uploads).values(upload).returning())[0];
 }
 
 export async function getUploadsByUserId(userId: string) {
--- packages/nextjs/src/actions/userActions.ts
+++ packages/nextjs/src/actions/userActions.ts
@@ -5,11 +5,24 @@ import { users } from "../db/schema";
 import { eq } from "drizzle-orm";
 
 export default async function addUser(address: string) {
-  await db.insert(users).values({
-    address,
-  });
+  return (
+    await db
+      .insert(users)
+      .values({
+        address,
+      })
+      .returning()
+  )[0];
 }
 
 export async function getUser(address: string) {
+  const usersData = await db.select().from(users).where(eq(users.address, address));
+  if (usersData.length === 0) {
+    return null;
+  }
+  return usersData[0];
+}
+
+export async function getUserByAddress(address: string) {
   return (await db.select().from(users).where(eq(users.address, address)))[0];
 }
