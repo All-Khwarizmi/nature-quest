57ee795129fa249d7f920e5d84bb91e116aadc35
Amarin Sam <amarinsam@gmail.com>
2025-01-26 16:09:43 +0700

    WIP: updating types

--- packages/nextjs/services/quest-agent/types.ts
+++ packages/nextjs/services/quest-agent/types.ts
@@ -2,6 +2,7 @@ import { ACTIVE, COMPLETED, EXPIRED } from "./constants";
 
 export type Reward = object;
 export type Status = typeof ACTIVE | typeof COMPLETED | typeof EXPIRED;
+export type Requirement = object;
 
 export type Quest = {
     id: string;
@@ -10,6 +11,10 @@ export type Quest = {
     description: string;
     reward: Reward;
     status: Status;
+    requirements: Requirement;
+    is_public: boolean;
+    created_at: number;
+    expires_at: number;
 }
 
 export interface IQuestAgent {
