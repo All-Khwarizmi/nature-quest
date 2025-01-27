3f3d0edfda7a4d55d0be28e45669999bef64d0a2
TEJASVI <1bi23ee060@bit-bangalore.edu.in>
2025-01-27 15:43:29 +0530

    Create natureToken.sol

---
+++ packages/hardhat/contracts/natureToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NatureToken is ERC20, Ownable {
+    address public authorizedMinter;
+    
+    constructor(address _authorizedMinter) ERC20("NATURE", "NTR") Ownable(msg.sender) {
+        require(_authorizedMinter != address(0), "Authorized minter cannot be zero address");
+        authorizedMinter = _authorizedMinter;
+    }
+    
+    modifier onlyAuthorizedMinter() {
+        require(msg.sender == authorizedMinter, "Only authorized minter can call this function");
+        _;
+    }
+    
+    function suck(uint256 amount) public onlyAuthorizedMinter {
+        _mint(msg.sender, amount);  // Mints directly to the caller's address
+    }
+}
