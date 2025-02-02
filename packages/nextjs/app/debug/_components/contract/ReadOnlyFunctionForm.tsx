af5af22a0fc805167aae0c5f8549de503730ef9a
Jason Suarez <suarezjason@icloud.com>
2025-02-02 08:39:47 +0100

    fix: bg color contrast

--- packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
+++ packages/nextjs/app/debug/_components/contract/ReadOnlyFunctionForm.tsx
@@ -71,7 +71,7 @@ export const ReadOnlyFunctionForm = ({
 
   return (
     <div className="flex flex-col gap-3 py-5 first:pt-0 last:pb-1">
-      <p className="font-medium my-0 break-words">
+      <p className="font-medium my-0  break-words">
         {abiFunction.name}
         <InheritanceTooltip inheritedFrom={inheritedFrom} />
       </p>
@@ -79,9 +79,9 @@ export const ReadOnlyFunctionForm = ({
       <div className="flex flex-col md:flex-row justify-between gap-2 flex-wrap">
         <div className="flex-grow w-full md:max-w-[80%]">
           {result !== null && result !== undefined && (
-            <div className="bg-secondary rounded-3xl text-sm px-4 py-1.5 break-words overflow-auto">
+            <div className="bg-primary rounded-3xl text-success text-sm px-4 py-1.5 break-words overflow-auto">
               <p className="font-bold m-0 mb-1">Result:</p>
-              <pre className="whitespace-pre-wrap break-words">{displayTxResult(result, "sm")}</pre>
+              <pre className="whitespace-pre-wrap  break-words">{displayTxResult(result, "sm")}</pre>
             </div>
           )}
         </div>
--- packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
+++ packages/nextjs/app/debug/_components/contract/TxReceipt.tsx
@@ -9,13 +9,10 @@ export const TxReceipt = ({ txResult }: { txResult: TransactionReceipt }) => {
   const [txResultCopied, setTxResultCopied] = useState(false);
 
   return (
-    <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-0">
+    <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0  py-0">
       <div className="mt-1 pl-2">
         {txResultCopied ? (
-          <CheckCircleIcon
-            className="ml-1.5 text-xl font-normal text-base-content h-5 w-5 cursor-pointer"
-            aria-hidden="true"
-          />
+          <CheckCircleIcon className="ml-1.5 text-xl font-normal  h-5 w-5 cursor-pointer" aria-hidden="true" />
         ) : (
           <CopyToClipboard
             text={JSON.stringify(txResult, replacer, 2)}
@@ -32,10 +29,10 @@ export const TxReceipt = ({ txResult }: { txResult: TransactionReceipt }) => {
       </div>
       <div className="flex-wrap collapse collapse-arrow">
         <input type="checkbox" className="min-h-0 peer" />
-        <div className="collapse-title text-sm min-h-0 py-1.5 pl-1 after:!top-4">
+        <div className="collapse-title text-success text-sm min-h-0 py-1.5 pl-1 after:!top-4">
           <strong>Transaction Receipt</strong>
         </div>
-        <div className="collapse-content overflow-auto bg-secondary rounded-t-none rounded-3xl !pl-0">
+        <div className="collapse-content text-white overflow-auto  rounded-t-none rounded-3xl !pl-0">
           <pre className="text-xs">
             {Object.entries(txResult).map(([k, v]) => (
               <ObjectFieldDisplay name={k} value={v} size="xs" leftPad={false} key={k} />
